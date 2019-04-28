879e5436878b39b6700bfe183bc8837002ec67a7
Tony <tsaechao19@gmail.com>
2019-04-27 18:22:41 -0700

    fixed spelling error for category

--- models/catagory.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-var catagorySchema = mongoose.Schema({
-    name: String,
-    details: String
-});
-
-module.exports = mongoose.model("Catagory", catagorySchema);
