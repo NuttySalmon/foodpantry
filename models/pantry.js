3f934a274c5df987202eb30a4d6d109086eefc4e
NuttySalmon <nuttysalmon@gmail.com>
2019-04-28 09:40:03 -0700

    Added form for registering pantry. Changed public url routing

--- models/pantry.js
+++ models/pantry.js
@@ -3,7 +3,8 @@ var mongoose = require("mongoose");
 var pantrySchema = mongoose.Schema({
     name: String,
     address: String,
-    phone: String
+    phone: String,
+    email: String
 });
 
 module.exports = mongoose.model("Pantry", pantrySchema);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 //Comments Create
 var express = require("express"),
     router = express.Router();
-router.get("/addpantry", function(req, res){
-  res.render("addpantry")
+router.get("/register", function(req, res){
+  res.render("register")
 });
 
 module.exports = router;
